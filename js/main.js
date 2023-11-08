da616c61f50b88717fe0605f5a41ec4f3e6f9d5d
gunmentoy <tomnguyen0443@gmail.com>
2023-11-08 01:15:16 -0800

    added, styled and added js functions to the faq section

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// FAQ accordion
+
+// waiting for the content of document is loaded
+document.addEventListener("DOMContentLoaded", () => {
+    // grabbing faq container
+    const faqContainer = document.querySelector(".faq-content");
+
+    // listening for a click on that container
+    faqContainer.addEventListener("click", (e) => {
+    // getting the closest header to where the click happened
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    // making sure that it returns nothing if clicked on body
+    if (!groupHeader) return;
+
+    // getting the group itself
+    const group = groupHeader.parentElement;
+
+    // getting the group body from the group selected above
+    const groupBody = group.querySelector(".faq-group-body");
+
+    // getting the icon for closing and opening
+    const icon = groupHeader.querySelector("i");
+
+    // toggling icon
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    // toggling visibility of faq's body
+    groupBody.classList.toggle("open");
+
+    // close other opened faq bodies
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+
+    // looping through other groups
+    otherGroups.forEach((otherGroup) => {
+      // making sure that the other group is not equal to the group that's clicked on
+        if (otherGroup !== group) {
+        // getting other group body
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        // getting other icon
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+
+        // removing the open class from other group body
+        otherGroupBody.classList.remove("open");
+        // removing the minus icon
+        otherIcon.classList.remove("fa-minus");
+        // adding the plus icon
+        otherIcon.classList.add("fa-plus");
+        }});
+    });
+});
